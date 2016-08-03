a1041b18c7f2142adbe83cc6941d4be3b6df8fa5
Rudi Yardley <contact@rudiyardley.com>
2016-08-03 10:41:27 +0700

    Basic CLI is working

---
+++ bin/bin.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+import program from 'commander';
+import pkg from '../package.json';
+import indexr from '../dist';
+
+const collect = (item, memo) => {
+  memo.push(item);
+  return memo;
+};
+
+const bool = (value) => !!value;
+const string = (value) => `${value}`;
+
+program
+  .version(`Indexr v${pkg.version}`)
+  .usage('<inputfolder> [options]')
+  .option('-o, --out [filename]',
+    'output filename for the file. ', string, undefined)
+  .option('-i, --include [glob]',
+    'a glob that will determine which folders are included as imports.', collect, [])
+  .option('-5, --es5',
+    'a glob that will determine which folders are included as imports.')
+  .option('-d, --direct-import',
+    'include the searched files in the import statements.')
+  .parse(process.argv);
+
+if (program.args.length === 0) program.help();
+
+const inputFolder = program.args[0];
+const outputFilename = program.out;
+const options = {};
+
+if (program.include.length > 0)
+  options.include = program.include;
+
+if (program.es5 !== undefined)
+  options.es5 = program.es5;
+
+if (program.directImport !== undefined)
+  options.directImport = program.directImport;
+
+console.log({
+  inputFolder,
+  outputFilename,
+  options,
+});
+
+const output = indexr(inputFolder, outputFilename, options);
+
+if (!outputFilename) console.log(output);
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,8 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
+import watch from 'gulp-watch';
 
-gulp.task('default', () => {
-  return gulp.src('lib/index.js')
-    .pipe(babel())
-    .pipe(gulp.dest('dist'));
-});
+gulp.task('default', () => watch(['lib/index.js', 'bin/bin.js'], { ignoreInitial: false })
+  .pipe(babel())
+  .pipe(gulp.dest('dist')));
+
