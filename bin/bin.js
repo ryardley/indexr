50b035fa590cc19f1fe4e4b2909886b901b9f419
Rudi Yardley <contact@rudiyardley.com>
2016-08-03 15:13:12 +0700

    Update CLI input.

--- bin/bin.js
+++ bin/bin.js
@@ -14,13 +14,22 @@ program
   .usage('<rootFolder> [options]')
   .option('-o, --out [filename]', 'The name of the output file. This file will be added to each module folder.')
   .option('-m, --modules [glob]',
-    'a glob pathed to the rootFolder that will determine which folders are module folders. If this is ommitted only the root folder is a module folder.')
+    'A glob pathed to the rootFolder that will determine which folders are module folders. If this is ommitted only the root folder is a module folder.')
   .option('-s, --submodules [glob]',
-    'a glob pathed to each module folder that will determine which submodules are imported to the index.', collect, [])
+    'A glob pathed to each module folder that will determine which submodules are imported to the index.', collect, [])
   .option('-5, --es5', 'Use ES5 template for index output.')
   .option('-d, --direct-import',
-    'include the searched files in the import statements.')
-  .parse(process.argv);
+    'Include the searched files in the import statements.')
+  .option('-i, --include [glob]', 'Deprecated in favour of --submodules');
+
+program.on('--help', () => {
+  console.log('  Examples:');
+  console.log('');
+  console.log('    $ indexr ./app --out server.js --modules **/modules/ --submodules */server.js');
+  console.log('');
+});
+
+program.parse(process.argv);
 
 if (program.args.length === 0) program.help();
 
@@ -28,14 +37,17 @@ const inputFolder = program.args[0];
 const outputFilename = program.out;
 const options = {};
 
-if (program.include.length > 0)
-  options.include = program.include;
+if (program.submodules.length > 0)
+  options.submodules = program.submodules;
 
 if (program.es5 !== undefined)
   options.es5 = program.es5;
 
 if (program.directImport !== undefined)
   options.directImport = program.directImport;
+
+if (program.modules !== undefined)
+  options.modules = program.modules;
 
 const output = indexr(inputFolder, outputFilename, options);
 
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,7 +2,7 @@ import gulp from 'gulp';
 import babel from 'gulp-babel';
 import watch from 'gulp-watch';
 
-gulp.task('default', () => watch(['lib/index.js', 'bin/bin.js'], { ignoreInitial: false })
+gulp.task('default', () => watch(['lib/*', 'bin/bin.js'], { ignoreInitial: false })
   .pipe(babel())
   .pipe(gulp.dest('dist')));
 
