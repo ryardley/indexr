7ff17f09348cc39894388ed2aa620d34ed2703c3
Rudi Yardley <contact@rudiyardley.com>
2016-08-12 15:06:54 +0700

    allowed for reecirsive files in build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,12 +2,12 @@ import gulp from 'gulp';
 import babel from 'gulp-babel';
 import watch from 'gulp-watch';
 
-gulp.task('watch', () => watch(['lib/*', 'bin/bin.js'], { ignoreInitial: false })
+gulp.task('watch', () => watch(['lib/**/*', 'bin/bin.js'], { ignoreInitial: false })
   .pipe(babel())
   .pipe(gulp.dest('dist')));
 
-gulp.task('compile', () => gulp.src(['lib/*', 'bin/bin.js'])
+gulp.task('compile', () => gulp.src(['lib/**/*', 'bin/bin.js'])
   .pipe(babel())
   .pipe(gulp.dest('dist')));
 
-gulp.task('default', ['watch'])
+gulp.task('default', ['watch']);
