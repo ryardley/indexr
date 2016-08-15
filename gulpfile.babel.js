23f579bcb6095f62973796a3b4861096d4fbe8d3
Rudi Yardley <contact@rudiyardley.com>
2016-08-16 02:53:05 +0700

    Eat own dogfood.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,10 +1,34 @@
-import gulp from 'gulp';
 import babel from 'gulp-babel';
+import gulp from 'gulp';
+import indexr from './lib';
+import path from 'path';
 import watch from 'gulp-watch';
+import chalk from 'chalk';
 
-gulp.task('watch', () => watch(['lib/**/*', 'bin/bin.js'], { ignoreInitial: false })
+const watchGlobs = ['lib/**/*', 'bin/bin.js'];
+const watchConfig = { ignored: '**/cliflags.js' };
+
+const runIndexr = () => indexr(path.resolve(__dirname, './lib'), {
+  modules: '**/modules/',
+  submodules: '*/cliflags.js',
+  outputFilename: 'cliflags.js',
+  directImport: true,
+});
+
+const babelTransform = () => gulp.src(watchGlobs)
   .pipe(babel())
-  .pipe(gulp.dest('dist')));
+  .pipe(gulp.dest('dist'));
+
+
+gulp.task('indexr', () => runIndexr());
+
+gulp.task('watch', ['indexr'], () => watch(watchGlobs, watchConfig, () => {
+  runIndexr()
+    .then(babelTransform)
+    .catch((err) => {
+      console.log(chalk.red(err));
+    });
+}));
 
 gulp.task('compile', () => gulp.src(['lib/**/*', 'bin/bin.js'])
   .pipe(babel())
