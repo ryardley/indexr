95985489fbf3d9674a5356b295af305c26d796b5
Rudi Yardley <contact@rudiyardley.com>
2016-08-16 00:03:48 +0700

    Modules Ignore globbing works!

--- test/indexr.js
+++ test/indexr.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import assert from 'assert';
 import defaultOptions from '../lib/modules/args/defaultOptions';
 import fs from 'fs';
@@ -8,6 +9,8 @@ import path from 'path';
 import sinon from 'sinon';
 import { Command } from 'commander';
 import { setLogLevel } from '../lib/utils/logger';
+
+// TODO: Simplify some of these examples
 
 // don't log stuff we dont care
 setLogLevel('none');
@@ -82,14 +85,10 @@ describe('indexr', () => {
       path.resolve(fractalFolder, 'modules', 'thing.js'),
       path.resolve(fractalFolder, 'modules', 'module-1', 'modules', 'foo.js'),
       path.resolve(fractalFolder, 'modules', 'module-1', 'modules', 'thing.js'),
-      path.resolve(fractalFolder, 'modules', 'module-1', 'modules', 'nested-2',
-        'modules', 'foo.js'),
-      path.resolve(fractalFolder, 'modules', 'module-1', 'modules', 'nested-2',
-        'modules', 'thing.js'),
-      path.resolve(fractalFolder, 'modules', 'module-1', 'modules', 'nested-2',
-        'modules', 'foo.js'),
-      path.resolve(fractalFolder, 'modules', 'module-1', 'modules', 'nested-2',
-        'modules', 'thing.js'),
+      path.resolve(fractalFolder, 'modules', 'module-1', 'modules', 'nested-2', 'modules', 'foo.js'),
+      path.resolve(fractalFolder, 'modules', 'module-1', 'modules', 'nested-2', 'modules', 'thing.js'),
+      path.resolve(fractalFolder, 'modules', 'module-1', 'modules', 'nested-2', 'modules', 'foo.js'),
+      path.resolve(fractalFolder, 'modules', 'module-1', 'modules', 'nested-2', 'modules', 'thing.js'),
       path.resolve(fractalFolder, 'modules', 'module-2', 'things', 'thing.js'),
     ];
 
@@ -228,22 +227,17 @@ describe('indexr', () => {
 
         const actual = [
           fs.readFileSync(path.resolve(fractalFolder, 'modules', 'foo.js'), 'utf-8'),
-          fs.readFileSync(path.resolve(fractalFolder, 'modules', 'module-1', 'modules',
-            'foo.js'), 'utf-8'),
-          fs.readFileSync(path.resolve(fractalFolder, 'modules', 'module-1', 'modules',
-            'nested-2', 'modules', 'foo.js'), 'utf-8'),
+          fs.readFileSync(path.resolve(fractalFolder, 'modules', 'module-1', 'modules', 'foo.js'), 'utf-8'),
+          fs.readFileSync(path.resolve(fractalFolder, 'modules', 'module-1', 'modules', 'nested-2', 'modules', 'foo.js'), 'utf-8'),
         ];
 
         assert.deepEqual(expected, actual, 'Function did not return expected output.');
         endTest();
       })
       .catch(endTest);
-
-      // assert(warnFunc.called);
     });
 
     it('should accept multiple globs in arrays and run indexr on each returned result', (endTest) => {
-
       indexr(fractalFolder, 'thing.js', {
         submodules: '*/index.js',
         modules: ['**/modules/', '**/things/'],
@@ -258,12 +252,9 @@ describe('indexr', () => {
 
         const actual = [
           fs.readFileSync(path.resolve(fractalFolder, 'modules', 'thing.js'), 'utf-8'),
-          fs.readFileSync(path.resolve(fractalFolder, 'modules', 'module-1', 'modules',
-            'thing.js'), 'utf-8'),
-          fs.readFileSync(path.resolve(fractalFolder, 'modules', 'module-1', 'modules',
-            'nested-2', 'modules', 'thing.js'), 'utf-8'),
-          fs.readFileSync(path.resolve(fractalFolder, 'modules', 'module-2', 'things',
-            'thing.js'), 'utf-8'),
+          fs.readFileSync(path.resolve(fractalFolder, 'modules', 'module-1', 'modules', 'thing.js'), 'utf-8'),
+          fs.readFileSync(path.resolve(fractalFolder, 'modules', 'module-1', 'modules', 'nested-2', 'modules', 'thing.js'), 'utf-8'),
+          fs.readFileSync(path.resolve(fractalFolder, 'modules', 'module-2', 'things', 'thing.js'), 'utf-8'),
         ];
 
         assert.deepEqual(expected, actual, 'Function did not return expected output.');
