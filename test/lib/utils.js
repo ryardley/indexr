47b941938c469bd790cfcf35733de688bd4736fc
Rudi Yardley <contact@rudiyardley.com>
2016-08-25 22:40:43 +0700

    Ignore node_modules folder by default.

---
+++ test/lib/utils.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import path from 'path';
+import { Command } from 'commander';
+import moduleParseCLI from '../../lib/modules/cli';
+
+export const tryer = (func, defval = false) => {
+  try {
+    return func();
+  } catch (e) {
+    return defval;
+  }
+};
+
+export const runCLI = (...cmd) =>
+  moduleParseCLI(['node', ...cmd], new Command());
+
+export const fileExists = (fileName) =>
+  tryer(() => fs.lstatSync(fileName).isFile());
+
+export const paths = {
+  inputFolder: path.resolve(__dirname, '..', './fixtures/input'),
+  fractalFolder: path.resolve(__dirname, '..', './fixtures/fractal'),
+  outputFolder: path.resolve(__dirname, '..', './fixtures/output'),
+};
+
