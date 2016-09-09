fe8cb5c08b4bd66c36f154d06e444e9691073245
Rudi Yardley <contact@rudiyardley.com>
2016-09-09 15:53:34 +1000

    style: Show log during tests to increase coverage to 100%

--- test/modules/logger.js
+++ test/modules/logger.js
@@ -4,7 +4,7 @@ import { resetLog, setLogLevel, logHistory, info } from '../../lib/utils/logger'
 export default () => {
   it('should actually log stuff and print to console', () => {
     resetLog();
-    setLogLevel('none');
+    setLogLevel('info'); // We need to write to the screen for test coverage
     info('foo');
     const actual = logHistory('info')[0];
     const expected = { level: 'info', message: 'indexr >> foo' };
